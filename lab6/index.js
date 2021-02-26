0396f41227f219dbb0475d29707802bcda5ec0e5
MagpieShrike <52262553+MagpieShrike@users.noreply.github.com>
2021-02-26 17:28:28 -0500

    Finish

---
+++ lab6/index.js
@@ -0,0 +1,22 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+
+const person = {
+    name: "Halie",
+    color: "Blue",
+};
+
+App.get("/", function(req, res) {
+    res.send("Hello World!");
+});
+
+App.get("/person", function (req, res) {
+    res.json(person);
+});
+
+App.listen(port, function() {
+    console.log("Server is running!");
+});
+
